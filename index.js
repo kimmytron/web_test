fc3df3efd1158bb9dc2cc673d4338b8d800da2e6
kimmytron <aiden.kim420@gmail.com>
2025-05-22 19:48:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function showprojects(){
     $("#projects_container").css("display","inherit");
-    $("#projects_container").removeClass("animated slideInDown");
-    void $("#projects_container")[0].offsetWidth;
     $("#projects_container").addClass("animated slideInDown");
     setTimeout(function(){
         $("#projects_container").removeClass("animated slideInDown");
     },800);
 }
-
 function closeprojects(){
     $("#projects_container").addClass("animated slideOutUp");
     setTimeout(function(){
@@ -15,17 +12,13 @@ function closeprojects(){
         $("#projects_container").css("display","none");
     },800);
 }
-
 function showabout(){
     $("#about_container").css("display","inherit");
-    $("#about_container").removeClass("animated slideInLeft");
-    void $("#about_container")[0].offsetWidth;
     $("#about_container").addClass("animated slideInLeft");
     setTimeout(function(){
         $("#about_container").removeClass("animated slideInLeft");
     },800);
 }
-
 function closeabout(){
     $("#about_container").addClass("animated slideOutLeft");
     setTimeout(function(){
@@ -33,17 +26,13 @@ function closeabout(){
         $("#about_container").css("display","none");
     },800);
 }
-
 function showwork(){
     $("#work_container").css("display","inherit");
-    $("#work_container").removeClass("animated slideInRight");
-    void $("#work_container")[0].offsetWidth;
     $("#work_container").addClass("animated slideInRight");
     setTimeout(function(){
         $("#work_container").removeClass("animated slideInRight");
     },800);
 }
-
 function closework(){
     $("#work_container").addClass("animated slideOutRight");
     setTimeout(function(){
@@ -51,17 +40,13 @@ function closework(){
         $("#work_container").css("display","none");
     },800);
 }
-
 function showcontact(){
     $("#contact_container").css("display","inherit");
-    $("#contact_container").removeClass("animated slideInUp");
-    void $("#contact_container")[0].offsetWidth;
     $("#contact_container").addClass("animated slideInUp");
     setTimeout(function(){
         $("#contact_container").removeClass("animated slideInUp");
     },800);
 }
-
 function closecontact(){
     $("#contact_container").addClass("animated slideOutDown");
     setTimeout(function(){
@@ -69,7 +54,6 @@ function closecontact(){
         $("#contact_container").css("display","none");
     },800);
 }
-
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
@@ -82,6 +66,3 @@ setTimeout(function(){
       $("#work").removeClass("animated fadeIn");
     },1000);
 },1500);
-
-
-
